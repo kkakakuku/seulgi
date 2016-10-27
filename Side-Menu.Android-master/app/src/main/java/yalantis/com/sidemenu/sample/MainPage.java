406ef48a60372cac3339e529d05240e5196188f1
kkakakuku <nattaa42@gmail.com>
2016-10-27 16:36:51 +0900

    side_bar
    side_bar

---
+++ Side-Menu.Android-master/app/src/main/java/yalantis/com/sidemenu/sample/MainPage.java
@@ -0,0 +1,15 @@
+package yalantis.com.sidemenu.sample;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainPage extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+
+
+}
